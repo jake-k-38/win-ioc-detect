f0967e917f2ba460bc23445fa3626677218a96a3
James Kelly <96206536+jake-k-38@users.noreply.github.com>
2021-12-22 20:56:29 -0500

    Update log4jcheckWIN.ps1
    Added "/Basic/Command/Base64" as IOC pattern
    ''{date:'j'}${date:'n'}${date:'d'}${date:'i'}:${date:'l'}${date:'d'}${date:'a'}${date:'p'}''
    
    Added common IOCs for APTs to check for
    
    Added ScheduledTasks scan to detect threats creating tasks, checks plain text and base64 equivalent
    
    Added outputGrid + optional saveToFile
    
    
    https://blog.cloudflare.com/actual-cve-2021-44228-payloads-captured-in-the-wild/
    https://github.com/Puliczek/CVE-2021-44228-PoC-log4j-bypass-words

--- log4jcheckWIN.ps1
+++ log4jcheckWIN.ps1
@@ -1,37 +1,59 @@
 $IOCPatterns = ('jndi:ldap:',
- 'jndi:rmi:/',
- 'jndi:ldaps:/',
- 'jndi:dns:/',
- 'jndi:nis:/',
- 'jndi:nds:/',
- 'jndi:corba:/',
- 'jndi:iiop:/',
- 'jndi:http:/',
- 'log4j',
- 'jndi.LDAPRefServer',
- 'jndi:ldap://',
- 'env:BARFOO:-j',
- 'env:BARFOO:-:',
- 'env:BARFOO:-l',
- 'env:BARFOO:-:',
- 'jndi',
- 'jndi:ld',
- 'jndi:ldap://127.0.0.1:1099/obj',
- 'at java.naming/com.sun.jndi.url.ldap.ldapURLContext.lookup',
- 'log4j.core.lookup.JndiLookup.lookup',
- 'Reference Class Name: foo',
- 'base64:')
- 
- $log4j_temp = ''
- $log4j_adtemp = ''
- $log4j_base64 = ''
- $log4j_base64ad = ''
- $datestring = (Get-Date).ToString('s').Replace(':','-') 
- 
- $saveToFile = 'True'
- $savedir = 'C:\sus_log4jScan_' + $datestring + '.txt' 
- $savedir2 = 'C:\sus_log4jScanBase64_' + $datestring + '.txt'
- 
+  'jndi:rmi:/',
+  'jndi:ldaps:/',
+  'jndi:dns:/',
+  'jndi:nis:/',
+  'jndi:nds:/',
+  'jndi:corba:/',
+  'jndi:iiop:/',
+  'log4j',
+  'jndi.LDAPRefServer',
+  'jndi:ldap://',
+  'env:BARFOO:-j',
+  'env:BARFOO:-:',
+  'env:BARFOO:-l',
+  'env:BARFOO:-:',
+  'jndi',
+  'jndi:ld',
+  'ldap:/',
+  'jndi:ldap://127.0.0.1:1099',
+  '{date:'j'}${date:'n'}${date:'d'}${date:'i'}:${date:'l'}${date:'d'}${date:'a'}${date:'p'}',
+  '/Basic/Command/Base64',
+  'at java.naming/com.sun.jndi.url.ldap.ldapURLContext.lookup',
+  'log4j.core.lookup.JndiLookup.lookup',
+  'Reference Class Name: foo',
+  'base64:',
+  '<Hidden>true</Hidden>', #Next 15 strings are common in APTs on infected machine
+  '-WindowStyle Hidden',
+  'IEX (New-Object Net.WebClient).DownloadString',
+  'base64_encode',
+  'base64_decode',
+  'cmd.exe /c',
+  'Scriptrunner.exe',
+  'Cscript.exe',
+  'WScript.exe',
+  'powershell.exe -exec bypass',
+  '-encodedcommand',
+  '-enc',
+  'VBscript.Encode',
+  'Wscript.Shell',
+  'LogonPasswords')
+
+$temp = ''
+$log4j_processtemp = ''
+$log4j_adtemp = ''
+$ioc_task = ''
+$ioc_base64 = ''
+$log4j_base64 = ''
+$log4j_base64ad = ''
+$datestring = (Get-Date).ToString('s').Replace(':','-') 
+
+$savedir = 'C:\ioc_log4jScan_' + $datestring + '.txt' 
+$savedir2 = 'C:\ioc_log4jScanBase64_' + $datestring + '.txt'
+
+$outputGrid = 'True'
+$saveToFile = 'False'
+
 #///////////////////////////////////////////////////////////////////////////////////////////////////
 # *** THIS MODULE IS REQUIRED FOR SCRIPT TO RUN ***
 # Get-Base64RegularExpression https://www.leeholmes.com/searching-for-content-in-base-64-strings/
@@ -43,74 +65,91 @@ $IOCPatterns = ('jndi:ldap:',
 # Got the idea from https://github.com/Neo23x0/log4shell-detector
 # You can add IOC patterns as the obfuscation of the reverse shells get tougher and more sophisticated
 
-$filter = @{
+$processfilter = @{
   LogName = 'Security'
   ID = 4688 #New process eventID
   StartTime = [datetime]::Now.AddHours(-24) #How far to look back in logs
 }
 
-$adfsfilter = @{ 
+$taskfilter = @{
+  LogName = 'Security'
+  ID = 4698 #New ScheduledTasks
+  StartTime = [datetime]::Now.AddHours(-24) #How far to look back in logs
+}
+
+$adfsfilter = @{
   LogName = 'AD FS Auditing'
   ID = 403 #RequestReceivedSuccessAudit
   StartTime = [datetime]::Now.AddHours(-24) #How far to look back in logs
 }
 
-$Width = -1 * ((measure-object -maximum length).maximum + 1)
+$Width = -1 * ((Measure-Object -Maximum length).maximum + 1)
 
-filter MultiSelect-String( [string[]]$Patterns ) {
-  if($_ -match "lower" -Or $_ -match "upper"){
-	  $obfusCheck = $_.Split('}$').Split(':')
-  }else{
-	  $obfusCheck = $_.Split('}$').Split('-')
+filter MultiSelect-String ([string[]]$Patterns) {
+  if ($_ -match '}$' -and ($_ -match "lower" -or $_ -match "upper")) {
+    $obfusCheck = $_.Split('}$').Split(':')
+  } elseif ($_ -match '}$') {
+    $obfusCheck = $_.Split('}$').Split('-')
   }
   $obfusString = ""
-  for($i = 0; $i -lt $obfusCheck.Length; $i++){
-	  if($obfusCheck[$i].Length -ne 1){
-		  continue
-	  }
-	  $obfusString += -join($obfusCheck[$i])
+  for ($i = 0; $i -lt $obfusCheck.Length; $i++) {
+    if ($obfusCheck[$i].Length -ne 1) {
+      continue
+    }
+    $obfusString += -join ($obfusCheck[$i])
   }
-  foreach( $Pattern in $Patterns ) {
+  foreach ($Pattern in $Patterns) {
+	if($_ | Select-String 'Task Name'){$temp = $_} #filter ScheduledTasks and save name
     # If one of the patterns does not match, continue checking same item.
-	if($_ | Select-String -AllMatches -Pattern $Pattern){
-		$_ #We found a match!
-	}elseif($obfusString | Select-String -AllMatches -Pattern $Pattern){ #check if hidden strings
-		Write-Warning("*OBFUSCATION FOUND* Matched: {0,$Width} {1}" -f $Pattern, $obfusString) 
-		"*OBFUSCATION FOUND* Matched: {0,$Width} {1}" -f $Pattern, $obfusString | out-file -Append -FilePath $savedir
-	}else{
-		continue
-	}
+    if ($_ | Select-String -AllMatches -Pattern $Pattern) {
+	  if($temp -ne ""){$temp}
+      $_ #We found a match!
+	  $temp = ''
+    } elseif ($obfusString | Select-String -AllMatches -Pattern $Pattern) { #check if hidden strings
+      Write-Warning ("*OBFUSCATION FOUND* Matched: {0,$Width} {1}" -f $Pattern, $obfusString)
+    } else {
+      continue
+    }
   }
 }
-filter MultiSelect-Base64String( [string[]]$Patterns ) {
+filter MultiSelect-Base64String ([string[]]$Patterns) {
   # Check the current item against all patterns.
-  foreach( $Pattern in $Patterns ) {
+  foreach ($Pattern in $Patterns) {
     # If one of the patterns does not match, continue checking same item.
-	$regex = (Get-Base64RegularExpression $Pattern)
-	if($_ | Select-String -AllMatches -Pattern $regex){
-		$_ #We found a match!
-		write-warning ("*BASE64 FOUND* Matched: {0,$Width} {1}" -f $Pattern, $_)
-		"*BASE64 FOUND* Matched: {0,$Width} {1}" -f $Pattern, $_ | out-file -Append -FilePath $savedir2
-	}else{ #Keep scanning
-		continue
-	}
+    $regex = (Get-Base64RegularExpression $Pattern)
+    if ($_ | Select-String -AllMatches -Pattern $regex) {
+      $_ #We found a match!
+      Write-Warning ("*BASE64 FOUND* Matched: {0,$Width} {1}" -f $Pattern, $_)
+    } else { #Keep scanning
+      continue
+    }
   }
 }
 
-Write-Warning("Checking for suspicious log4j events (EventID 4688, ADFS 403):")
-$log4j_temp = Get-WinEvent -FilterHashtable $filter | Select-Object -ExpandProperty Message | MultiSelect-String $IOCPatterns
-$log4j_adtemp = Get-WinEvent -FilterHashtable $adfsfilterfilter | Select-Object -ExpandProperty Message | MultiSelect-String $IOCPatterns
-Write-Output($log4j_temp)
-Write-Output($log4j_adtemp)
+Write-Warning ("Checking for suspicious log4j events/IOCs (EventID 4688, EventID 4698, ADFS 403):")
+$log4j_processtemp = Get-WinEvent -FilterHashtable $processfilter -ErrorAction Continue | Select-Object -ExpandProperty Message | MultiSelect-String $IOCPatterns
+$ioc_task = Get-WinEvent -FilterHashtable $taskfilter -ErrorAction Continue | Select-Object -Property * | Out-String -Stream | Select-String -Pattern 'Task Name','<Hidden>','<Command>','<Arguments>' | MultiSelect-String $IOCPatterns
+$log4j_adtemp = Get-WinEvent -FilterHashtable $adfsfilter -ErrorAction Continue | Select-Object -ExpandProperty Message | MultiSelect-String $IOCPatterns
 
-Write-Warning("Checking for suspicious Base64 encoded log4j events:")
-$log4j_base64 = Get-WinEvent -FilterHashtable $filter | Select-Object -ExpandProperty Message | MultiSelect-Base64String $IOCPatterns
-$log4j_base64ad = Get-WinEvent -FilterHashtable $filter | Select-Object -ExpandProperty Message | MultiSelect-Base64String $IOCPatterns
+Write-Warning ("Checking for suspicious Base64 encoded log4j events/IOCs:")
+$log4j_base64 = Get-WinEvent -FilterHashtable $processfilter -ErrorAction Continue | Select-Object -ExpandProperty Message | MultiSelect-Base64String $IOCPatterns
+$log4j_base64ad = Get-WinEvent -FilterHashtable $adfsfilter -ErrorAction Continue | Select-Object -ExpandProperty Message | MultiSelect-Base64String $IOCPatterns
+$ioc_base64 = Get-WinEvent -FilterHashtable $taskfilter -ErrorAction Continue | Select-Object -Property * | Out-String -Stream | Select-String -Pattern 'Task Name','<Hidden>','<Command>','<Arguments>' | MultiSelect-Base64String $IOCPatterns
 
+if($outputGrid){
+  if($log4j_processtemp -ne ""){$log4j_processtemp | Out-GridView -Title 'IOCs EventID 4688'}
+  if($log4j_adtemp -ne ""){$log4j_adtemp | Out-GridView -Title 'IOCs ADFS 403'}
+  if($ioc_task -ne ""){$ioc_task | Out-GridView -Title 'IOCs EventID 4698'}
+  if($log4j_base64 -ne ""){$log4j_base64 | Out-GridView -Title 'Base64 IOCs EventID 4688'}
+  if($ioc_base64 -ne ""){$ioc_base64 | Out-GridView -Title 'Base64 IOCs EventID 4698'}
+  if($log4j_base64ad -ne ""){$log4j_base64ad | Out-GridView -Title 'Base64 IOCs ADFS 403'}
+}
 
 if($saveToFile){
 	$log4j_temp | out-file -Append -FilePath $savedir
 	$log4j_adtemp | out-file -Append -FilePath $savedir
+	$ioc_task | out-file -Append -FilePath $savedir
 	$log4j_base64 | out-file -Append -FilePath $savedir2
+	$ioc_base64 | out-file -Append -FilePath $savedir2
 	$log4j_base64ad | out-file -Append -FilePath $savedir2
 }
