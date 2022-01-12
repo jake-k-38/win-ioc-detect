c772b5676287d36e7e5a0da1136c7300878ef519
James Kelly <96206536+jake-k-38@users.noreply.github.com>
2022-01-11 20:51:27 -0500

    Update log4jcheckWIN.ps1
    Added some more generic IOC patterns for schtask including hidden windows for powershell like /w 01 and common directory for placing persistence

--- log4jcheckWIN.ps1
+++ log4jcheckWIN.ps1
@@ -17,27 +17,33 @@ $IOCPatterns = ('jndi:ldap:',
   'jndi:ld',
   'ldap:/',
   'jndi:ldap://127.0.0.1:1099',
-  '{date:'j'}${date:'n'}${date:'d'}${date:'i'}:${date:'l'}${date:'d'}${date:'a'}${date:'p'}',
+  '{date:j}${date:n}${date:d}${date:i}:${date:l}${date:d}${date:a}${date:p}',
   '/Basic/Command/Base64',
   'at java.naming/com.sun.jndi.url.ldap.ldapURLContext.lookup',
   'log4j.core.lookup.JndiLookup.lookup',
   'Reference Class Name: foo',
   'base64:',
-  '<Hidden>true</Hidden>', #Next 15 strings are common in APTs on infected machine
+  '<Hidden>true</Hidden>',#Next 15 strings are common in APTs in schtasks on infected machines
   '-WindowStyle Hidden',
+  'powershell/w 01',
+  'powershell/w 01 /ep 0/nop/c',
+  'powershell.exe -exec bypass',
+  'powershell -c iex',
   'IEX (New-Object Net.WebClient).DownloadString',
+  '-NonInteractive',
+  '-NoLogo',
+  '-ExecutionPolicy bypass',
+  '-encodedcommand',
+  '-enc',
+  'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp',
   'base64_encode',
   'base64_decode',
   'cmd.exe /c',
   'Scriptrunner.exe',
   'Cscript.exe',
   'WScript.exe',
-  'powershell.exe -exec bypass',
-  '-encodedcommand',
-  '-enc',
   'VBscript.Encode',
-  'Wscript.Shell',
-  'LogonPasswords')
+  'Wscript.Shell')
 
 $temp = ''
 $log4j_processtemp = ''
@@ -46,9 +52,9 @@ $ioc_task = ''
 $ioc_base64 = ''
 $log4j_base64 = ''
 $log4j_base64ad = ''
-$datestring = (Get-Date).ToString('s').Replace(':','-') 
+$datestring = (Get-Date).ToString('s').Replace(':','-')
 
-$savedir = 'C:\ioc_log4jScan_' + $datestring + '.txt' 
+$savedir = 'C:\ioc_log4jScan_' + $datestring + '.txt'
 $savedir2 = 'C:\ioc_log4jScanBase64_' + $datestring + '.txt'
 
 $outputGrid = 'True'
@@ -99,14 +105,13 @@ filter MultiSelect-String ([string[]]$Patterns) {
     $obfusString += -join ($obfusCheck[$i])
   }
   foreach ($Pattern in $Patterns) {
-	if($_ | Select-String 'Task Name'){$temp = $_} #filter ScheduledTasks and save name
-    # If one of the patterns does not match, continue checking same item.
+    if ($_ | Select-String 'Task Name') { $temp = $_ } #filter ScheduledTasks and save name
     if ($_ | Select-String -AllMatches -Pattern $Pattern) {
-	  if($temp -ne ""){$temp}
+      if ($temp -ne "") { $temp }
       $_ #We found a match!
-	  $temp = ''
+      $temp = ''
     } elseif ($obfusString | Select-String -AllMatches -Pattern $Pattern) { #check if hidden strings
-      Write-Warning ("*OBFUSCATION FOUND* Matched: {0,$Width} {1}" -f $Pattern, $obfusString)
+      Write-Warning ("*OBFUSCATION FOUND* Matched: {0,$Width} {1}" -f $Pattern,$obfusString)
     } else {
       continue
     }
@@ -119,7 +124,7 @@ filter MultiSelect-Base64String ([string[]]$Patterns) {
     $regex = (Get-Base64RegularExpression $Pattern)
     if ($_ | Select-String -AllMatches -Pattern $regex) {
       $_ #We found a match!
-      Write-Warning ("*BASE64 FOUND* Matched: {0,$Width} {1}" -f $Pattern, $_)
+      Write-Warning ("*BASE64 FOUND* Matched: {0,$Width} {1}" -f $Pattern,$_)
     } else { #Keep scanning
       continue
     }
@@ -136,20 +141,20 @@ $log4j_base64 = Get-WinEvent -FilterHashtable $processfilter -ErrorAction Contin
 $log4j_base64ad = Get-WinEvent -FilterHashtable $adfsfilter -ErrorAction Continue | Select-Object -ExpandProperty Message | MultiSelect-Base64String $IOCPatterns
 $ioc_base64 = Get-WinEvent -FilterHashtable $taskfilter -ErrorAction Continue | Select-Object -Property * | Out-String -Stream | Select-String -Pattern 'Task Name','<Hidden>','<Command>','<Arguments>' | MultiSelect-Base64String $IOCPatterns
 
-if($outputGrid){
-  if($log4j_processtemp -ne ""){$log4j_processtemp | Out-GridView -Title 'IOCs EventID 4688'}
-  if($log4j_adtemp -ne ""){$log4j_adtemp | Out-GridView -Title 'IOCs ADFS 403'}
-  if($ioc_task -ne ""){$ioc_task | Out-GridView -Title 'IOCs EventID 4698'}
-  if($log4j_base64 -ne ""){$log4j_base64 | Out-GridView -Title 'Base64 IOCs EventID 4688'}
-  if($ioc_base64 -ne ""){$ioc_base64 | Out-GridView -Title 'Base64 IOCs EventID 4698'}
-  if($log4j_base64ad -ne ""){$log4j_base64ad | Out-GridView -Title 'Base64 IOCs ADFS 403'}
+if ($outputGrid) {
+  if ($log4j_processtemp -ne "") { $log4j_processtemp | Out-GridView -Title 'IOCs EventID 4688' }
+  if ($log4j_adtemp -ne "") { $log4j_adtemp | Out-GridView -Title 'IOCs ADFS 403' }
+  if ($ioc_task -ne "") { $ioc_task | Out-GridView -Title 'IOCs EventID 4698' }
+  if ($log4j_base64 -ne "") { $log4j_base64 | Out-GridView -Title 'Base64 IOCs EventID 4688' }
+  if ($ioc_base64 -ne "") { $ioc_base64 | Out-GridView -Title 'Base64 IOCs EventID 4698' }
+  if ($log4j_base64ad -ne "") { $log4j_base64ad | Out-GridView -Title 'Base64 IOCs ADFS 403' }
 }
 
-if($saveToFile){
-	$log4j_temp | out-file -Append -FilePath $savedir
-	$log4j_adtemp | out-file -Append -FilePath $savedir
-	$ioc_task | out-file -Append -FilePath $savedir
-	$log4j_base64 | out-file -Append -FilePath $savedir2
-	$ioc_base64 | out-file -Append -FilePath $savedir2
-	$log4j_base64ad | out-file -Append -FilePath $savedir2
+if ($saveToFile) {
+  $log4j_temp | Out-File -Append -FilePath $savedir
+  $log4j_adtemp | Out-File -Append -FilePath $savedir
+  $ioc_task | Out-File -Append -FilePath $savedir
+  $log4j_base64 | Out-File -Append -FilePath $savedir2
+  $ioc_base64 | Out-File -Append -FilePath $savedir2
+  $log4j_base64ad | Out-File -Append -FilePath $savedir2
 }
